0b05615b2abe66ed67affe07e1e2ba43229c6dbc
ptr-s <rmsptr@yahoo.com>
2024-12-11 08:42:00 +0300

    add main code

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 import pygame
+import random
+from config import *
 
 pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Игра Тир")
+icon = pygame.image.load("images/shooter-art.jpg")
+pygame.display.set_icon(icon)
+
+target_image = pygame.image.load("images/apple70.png")
+target_width = 70
+target_height = 70
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+screen_color = (random.randint(50, 150), random.randint(50, 150), random.randint(50, 150))
 
 running = True
-
 while running:
-    pass
+    screen.fill(screen_color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+    screen.blit(target_image, (target_x, target_y))
+    pygame.display.update()
 
 pygame.quit()
